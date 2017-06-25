7317c21fdbee1fb535eaa469a7ab0c11b9b75868
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-25 14:43:45 -0600

    every other click

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 $(function() {
   'use strict';
 
+  var state = {
+    turnX: true
+  }
+
   var x = '<i class="fa fa-times" aria-hidden="true"></i>';
   var o = '<i class="fa fa-circle-o" aria-hidden="true"></i>';
 
   console.log("tic tac toe")
+
+  function squareClick(state) {
+    $('.game-container .js-open-square').click(function(){
+      if (state.turnX === true) {
+        state.turnX = false;
+        $(this).append(x)
+      }
+      else {
+        state.turnX = true;
+        $(this).append(o)
+      }
+      console.log(state);
+    });
+  }
+
+
+  squareClick(state);
 
 });
