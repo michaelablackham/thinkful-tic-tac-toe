24e9b70363b45eb97b01e3d8da654bb34bc64eb1
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-25 13:52:35 -0600

    setting up folder structure and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 
-gulp.task('build', 'build:css');
+gulp.task('build', ['build:css']);
 
 gulp.task('build:css', function () {
-  gulp.src('./src/scss/*.scss')
+  gulp.src('./scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./build'))
 })
 
 gulp.task('watch', function () {
   gulp.watch([
-    './src/scss/*.scss',
+    './scss/*.scss',
   ], ['build']);
 })
