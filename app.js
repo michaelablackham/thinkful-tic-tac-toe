ec4fafc7f3d05d5058e7188bd939766a338aeee4
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-25 14:18:21 -0600

    styling

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function() {
+  'use strict';
+
+  var x = '<i class="fa fa-times" aria-hidden="true"></i>';
+  var o = '<i class="fa fa-circle-o" aria-hidden="true"></i>';
+
+  console.log("tic tac toe")
+
+});
