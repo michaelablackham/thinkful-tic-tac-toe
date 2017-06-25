de97e82ce9f84bd3684f5ff42494c148815791c7
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-25 15:24:55 -0600

    updating state and logic

--- app.js
+++ app.js
@@ -2,28 +2,60 @@ $(function() {
   'use strict';
 
   var state = {
-    turnX: true
+    turnX: true,
+    currentClick: 0,
+    maxClick: 6
   }
 
-  var x = '<i class="fa fa-times" aria-hidden="true"></i>';
-  var o = '<i class="fa fa-circle-o" aria-hidden="true"></i>';
+  var x ='<i class="fa fa-times" aria-hidden="true"></i>';
+  var o ='<i class="fa fa-circle-o" aria-hidden="true"></i>';
 
-  console.log("tic tac toe")
+  // var WINNERS = {
+  //   rowTop: '.row1',
+  //   rowMiddle: '.row2',
+  //   rowBottom: '.row3',
+  //   col1: '.row .square(0)',
+  //   col2: ,
+  //   col3: ,
+  //   diagNeg: ,
+  //   diagPos:
+  // }
+
 
   function squareClick(state) {
-    $('.game-container .js-open-square').click(function(){
-      if (state.turnX === true) {
-        state.turnX = false;
-        $(this).append(x)
+    $('.game-container .square').click(function(){
+      if ($(this).hasClass('js-open-square')) {
+        updateState(state);
+        renderClick(state);
+        $(this).removeClass('js-open-square');
+        console.log(state)
       }
-      else {
-        state.turnX = true;
-        $(this).append(o)
-      }
-      console.log(state);
     });
   }
 
+  function renderClick(state) {
+    if (state.turnX === true) {
+      $(event.target).html(x).addClass('x-filled');
+    }
+    else {
+      $(event.target).html(o).addClass('o-filled');
+    }
+  }
+
+  function updateState(state) {
+    if (state.currentClick < state.maxClick) {
+      state.currentClick++;
+      if (state.turnX === true) {
+        state.turnX = false;
+      }
+      else {
+        state.turnX = true;
+      }
+    }
+    else if (state.currentClick === state.maxClick){
+      alert("you're out of moves!")
+    }
+  }
 
   squareClick(state);
 
