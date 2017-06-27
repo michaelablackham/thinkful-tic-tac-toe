d73906c92b9b8013091b93a1636a175d188de5c1
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-26 21:43:38 -0600

    logix to check for winner

--- app.js
+++ app.js
@@ -9,68 +9,62 @@ $(function() {
     ],
     turnO: false,
     currentClick: 0,
-    maxClick: 8
+    maxClick: 9
   }
 
-  function isWinner(state){
-    var firstColumn = true;
-    results = []
-    var results = [
-      [
-        state.board[0][0] && state.board[0][1] && state.board[0][2]
-        ||
-        !state.board[0][0] && !state.board[0][1] && !state.board[0][2],
-
-        state.board[0][0] && state.board[0][1] && state.board[0][2]
-        ||
-        !state.board[0][0] && !state.board[0][1] && !state.board[0][2],
-
-        state.board[0][0] && state.board[0][1] && state.board[0][2]
-        ||
-        !state.board[0][0] && !state.board[0][1] && !state.board[0][2]
-      ],
-      [
-        state.board[1][0] && state.board[1][1] && state.board[1][2]
-        ||
-        !state.board[1][0] && !state.board[1][1] && !state.board[1][2],
-
-        state.board[1][0] && state.board[1][1] && state.board[1][2]
-        ||
-        !state.board[1][0] && !state.board[1][1] && !state.board[1][2],
-
-        state.board[1][0] && state.board[1][1] && state.board[1][2]
-        ||
-        !state.board[1][0] && !state.board[1][1] && !state.board[1][2]
-      ],
-      [
-        state.board[2][0] && state.board[2][1] && state.board[2][2]
-        ||
-        !state.board[2][0] && !state.board[2][1] && !state.board[2][2],
-
-        state.board[2][0] && state.board[2][1] && state.board[2][2]
-        ||
-        !state.board[2][0] && !state.board[2][1] && !state.board[2][2],
-
-        state.board[2][0] && state.board[2][1] && state.board[2][2]
-        ||
-        !state.board[2][0] && !state.board[2][1] && !state.board[2][2]
-      ],
-    ]
-  }
+  // function isWinner(state){
+  //   var firstColumn = true;
+  //   results = []
+  //   var results = [
+  //     [
+  //       state.board[0][0] && state.board[0][1] && state.board[0][2]
+  //       ||
+  //       !state.board[0][0] && !state.board[0][1] && !state.board[0][2],
+  //
+  //       state.board[0][0] && state.board[0][1] && state.board[0][2]
+  //       ||
+  //       !state.board[0][0] && !state.board[0][1] && !state.board[0][2],
+  //
+  //       state.board[0][0] && state.board[0][1] && state.board[0][2]
+  //       ||
+  //       !state.board[0][0] && !state.board[0][1] && !state.board[0][2]
+  //     ],
+  //     [
+  //       state.board[1][0] && state.board[1][1] && state.board[1][2]
+  //       ||
+  //       !state.board[1][0] && !state.board[1][1] && !state.board[1][2],
+  //
+  //       state.board[1][0] && state.board[1][1] && state.board[1][2]
+  //       ||
+  //       !state.board[1][0] && !state.board[1][1] && !state.board[1][2],
+  //
+  //       state.board[1][0] && state.board[1][1] && state.board[1][2]
+  //       ||
+  //       !state.board[1][0] && !state.board[1][1] && !state.board[1][2]
+  //     ],
+  //     [
+  //       state.board[2][0] && state.board[2][1] && state.board[2][2]
+  //       ||
+  //       !state.board[2][0] && !state.board[2][1] && !state.board[2][2],
+  //
+  //       state.board[2][0] && state.board[2][1] && state.board[2][2]
+  //       ||
+  //       !state.board[2][0] && !state.board[2][1] && !state.board[2][2],
+  //
+  //       state.board[2][0] && state.board[2][1] && state.board[2][2]
+  //       ||
+  //       !state.board[2][0] && !state.board[2][1] && !state.board[2][2]
+  //     ],
+  //   ]
+  //
+  //   var hasAWinner = results.reduce(function (acc, val) {
+  //     return acc || val[0] || val[1] || val[2]
+  //   }, false);
+  // }
 
   var x ='<i class="fa fa-times" aria-hidden="true"></i>';
   var o ='<i class="fa fa-circle-o" aria-hidden="true"></i>';
 
-  function squareClick(state) {
-    $('.game-container .square').click(function(){
-      if (!$(this).hasClass('js-open-square')) return;
-
-      updateState(state);
-      renderClick(state);
-      $(this).removeClass('js-open-square');
-      console.log(state);
-    });
-  }
 
   function renderClick(state) {
     if (state.turnO === true) {
@@ -85,19 +79,48 @@ $(function() {
     if (state.currentClick < state.maxClick) {
       state.currentClick++;
       state.turnO = !state.turnO;
-
-      // if (state.turnO === true) {
-      //   state.turnO = false;
-      // }
-      // else {
-      //   state.turnO = true;
-      // }
-    }
-    if (state.currentClick === state.maxClick){
-      alert("you're out of moves!")
     }
   }
 
-  squareClick(state);
+  function updateBoard(state) {
+    var rowIndex = $(event.target).parent().index();
+    var squareIndex = $(event.target).index();
+    state.board[rowIndex][squareIndex] = state.turnO;
+    console.log(state)
+
+    if ( state.board[rowIndex][0] && state.board[rowIndex][1] && state.board[rowIndex][2] === true ) {
+      console.log("0 wins!")
+    }
+    // else if ( state.board[rowIndex][0] && state.board[rowIndex][1] && state.board[rowIndex][2] == false ) {
+    //   console.log("x wins!")
+    // }
+  }
+
+  function boardCheck(state) {
+    // if (state.currentClick === state.maxClick){
+    //   alert("you're out of moves!")
+    // }
+  }
+
+  function isWinner(state) {
+    var rowIndex = $(event.target).parent().index();
+    var squareIndex = $(event.target).index();
+
+    // if ( state.board[rowIndex][0] && state.board[rowIndex][1] && state.board[rowIndex][2] == true ) {
+    //   console.log("0 wins!")
+    // }
+    // else if ( state.board[rowIndex][0] && state.board[rowIndex][1] && state.board[rowIndex][2] != true ) {
+    //   console.log("x wins!")
+    // }
+  }
+
+  $('.game-container .square').click(function(){
+    if (!$(this).hasClass('js-open-square')) return;
+    updateBoard(state);
+    updateState(state);
+    renderClick(state);
+    isWinner(state);
+    $(this).removeClass('js-open-square');
+  });
 
 });
